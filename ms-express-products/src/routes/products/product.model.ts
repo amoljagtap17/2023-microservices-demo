6cc6060b0f00bd948c3530c752a95715f0cddebd
amoljagtap17 <amoljagtap17@gmail.com>
2023-08-27 18:37:43 +0530

    microservices-demo: update mongoose model

--- ms-express-products/src/routes/products/product.model.ts
+++ ms-express-products/src/routes/products/product.model.ts
@@ -10,14 +10,20 @@ const productSchema = new Schema<IProduct>(
   {
     id: true,
     timestamps: true,
+    versionKey: false,
     toJSON: {
       transform(_doc, ret, _options) {
         ret.id = ret._id;
         delete ret._id;
-        delete ret.__v;
+        // delete ret.__v;
       },
     },
   }
 );
+
+/* productSchema.post("save", function (doc, next) {
+  console.log("post action on save", doc);
+  next();
+}); */
 
 export const Product = model<IProduct>("Product", productSchema);
