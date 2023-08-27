f7d8a39c78cc4ef86b5d9b2ce48ada44a6e7a132
amoljagtap17 <amoljagtap17@gmail.com>
2023-08-27 19:48:57 +0530

    microservices-demo: update product validator

--- ms-express-products/src/routes/products/validators/products.validators.ts
+++ ms-express-products/src/routes/products/validators/products.validators.ts
@@ -1,7 +1,11 @@
 import { body } from "express-validator";
 
 export const productsValidators = [
-  body("category").notEmpty().trim().withMessage("Category is required"),
-  body("name").notEmpty().trim().withMessage("Name is required"),
+  body("category")
+    .notEmpty()
+    .isString()
+    .trim()
+    .withMessage("Category is required"),
+  body("name").notEmpty().isString().trim().withMessage("Name is required"),
   body("price").notEmpty().isNumeric().withMessage("Price is required"),
 ];
